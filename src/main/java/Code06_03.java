70398b348079f719291be8b710d9768c32491dc1
yeeeehs <leeihs122@gmail.com>
2026-04-10 14:05:19 +0900

    Code06_03

---
+++ src/main/java/Code06_03.java
@@ -0,0 +1,7 @@
+public class Code06_03 {
+    public static void main(String[] args) {
+        for (int i=0; i<3; i++) {
+            System.out.println(i + ": 난생처음 자바는 재미있습니다. ^^");
+        }
+    }
+}
