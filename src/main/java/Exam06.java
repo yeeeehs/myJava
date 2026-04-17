641afe84f48f8c04b555f3daebde336c7cba78ce
yeeeehs <leeihs122@gmail.com>
2026-04-17 13:05:13 +0900

    쪽지시험6

--- src/main/java/Exam06.java
+++ src/main/java/Exam06.java
@@ -2,15 +2,13 @@ public class Exam06 {
     public static void main(String[] args){
         int[] nums = {1,3,7,1,4,7,3,1,9,0,7,3};
         int max = nums[0];
-        int min = nums[0];
 
         for (int i=0; i<nums.length; i++) {
             if (nums[i] > max) {
                 max = nums[i];
-            } else if (nums[i] < min) {
-                min = nums[i];
             }
         }
+
         System.out.println(max+"입니다.");
     }
 }
