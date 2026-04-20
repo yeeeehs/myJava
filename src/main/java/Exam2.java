228816affef326c926051bfc9828cd8355eb9fe3
yeeeehs <leeihs122@gmail.com>
2026-04-20 20:33:04 +0900

    1학기 중간고사 문제 2번

---
+++ src/main/java/Exam2.java
@@ -0,0 +1,13 @@
+public class Exam2 {
+
+    public static void main(String[] args) {
+
+        String log = "127.0.0.1 - frank [10/Oct/2000:13:55:36-0700]\"GET /apache_pb.gif HTTP/1.0\" 200";
+
+        int space = log.indexOf(" ", log.indexOf("\""));
+
+        String res = log.substring(space + 1, log.indexOf(" ", space + 1));
+
+        System.out.println("접속한 페이지 : " + res);
+    }
+}
