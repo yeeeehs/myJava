b466d8b4ed8c365263d1f23cc1fa832d9b9dd74e
yeeeehs <leeihs122@gmail.com>
2026-04-24 00:22:27 +0900

    1학기 중간고사 문제 2번

--- src/main/java/Exam2.java
+++ src/main/java/Exam2.java
@@ -7,14 +7,17 @@ public class Exam2 {
         // 웹 서버 로그 문자열 저장
         String log = "127.0.0.1 - frank [10/Oct/2000:13:55:36-0700]\"GET /apache_pb.gif HTTP/1.0\" 200";
 
-        // 큰따옴표(")위치를 먼저 찾고, 그 위치 이후에서 첫 번째 공백을 찾아 GET 뒤 공백 위치를 구함.
-        int space = log.indexOf(" ", log.indexOf("\""));
+        //큰따옴표(") 위치를 찾은 후, 그 위치 이후에서 첫번째 공백을 찾음
+        //+1을 통해 공백 다음 문자로 이동(url시작 위치)
+        int start = log.indexOf(" ", log.indexOf("\""))+1;
 
-        // GET 뒤 공백 다음 글자부터 시작해서
-        // 그 다음 공백 전까지 잘라내어 요청 URL(/apache_pb.gif)만 추출
-        String res = log.substring(space + 1, log.indexOf(" ", space + 1));
+        //start 위치 이후에서 첫 번째 공백을 찾아 요청 url의 끝 위치를 구한다
+        int end = log.indexOf(" ", start);
 
-        // 변수 res에 저장된 URL을 접속한 페이지 : /apache_pb.gif 형태로 추출
-        System.out.println("접속한 페이지 : " + res);
+        //start부터 end 직전까지 잘라서 요청 url만 추출
+        String url = log.substring(start,end);
+
+        // 변수 url에 저장된 값을 출력
+        System.out.println(url);
     }
 }
