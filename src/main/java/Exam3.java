2c60b6f8cddfabc286d75f10a0b3b6e490f19b89
yeeeehs <leeihs122@gmail.com>
2026-04-23 15:11:25 +0900

    1학기 중간고사 문제 3번

--- src/main/java/Exam3.java
+++ src/main/java/Exam3.java
@@ -1,14 +1,23 @@
-public class Exam3 { //Exam03이라는 클래스 선언
+public class Exam3 {
+    // Exam03이라는 클래스 선언
 
-    public static void main(String[] args) { //프로그램의 시작점인 main메서드
+    public static void main(String[] args) {
+        // 프로그램이 시작되는 메인 메서드
 
-        int hap = 0; //합계를 저장할 변수 hap을 선언 후 0으로 초기화
+        // 합계를 저장할 변수 hap을 선언 후 0으로 초기화
+        int hap = 0;
 
-        for (int i = 100; i <= 300; i++) { //i는 100부터 300까지 1씩 증가하며 반복하는 for문
-            if (i % 5 == 0 && i % 7 == 0) { //5의 배수, 7의 배수 둘 다 만족하는지
-                hap += i; //조건에 맞는 숫자 i를 hap에 더해줌
+        // i를 100부터 300까지 1씩 증가하며 반복하는 for문
+        for (int i = 100; i <= 300; i++) {
+
+            //&&(AND연산자)를 사용한 조건문. i가 5의 배수이면서 동시에 7의 배수인지 확인
+            if (i % 5 == 0 && i % 7 == 0) {
+
+                //조건에 만족하는 값 i를 hap에 더해줌
+                hap += i;
             }
         }
-        System.out.println(hap); //지금까지 더한 총합인 hap 출력
+        //for문이 다 돌고 난 후, 누적된 총합인 hap을 출력
+        System.out.println(hap);
     }
 }
